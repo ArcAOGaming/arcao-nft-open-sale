b88d4e39d1609e6d83f4bc24f7933b354d6cf9f6
K <111819113+KennySwayzee93@users.noreply.github.com>
2025-02-04 06:46:40 -0500

    Update token_constants.lua

--- src/constants/token_constants.lua
+++ src/constants/token_constants.lua
@@ -1,6 +1,15 @@
 local nft_process_ids = {
-    ["process-id-1"] = true,
-    ["process-id-2"] = true
+    ["ND9mS61GK2fBXlZU_eVFrXwubHYNBHCTMJd6KdtWbiI"] = true,
+    ["R9vilEqfERNwRw8M94coHfM7B1g-zpRV51BG5vOuWDE"] = true,
+    ["EKbxPhCJfRifSccyeJnApg6fqJ8EFqZhOENWFtCBLxw"] = true,
+    ["3pb1Bt7f9OEU4EXAQKGrdAiF950NuvRzxgjaDmjr7Aw"] = true,
+    ["oi10f-aOyKI0R3Y4QtTSIrA52eCOd7BdV9xu7LFVi28"] = true,
+    ["WzDJaLoGtazFc-M5lj9c-CUttYzhS7Vpjtfn4j9B2JE"] = true,
+    ["BonaQR0Y6wgn831vpYMmGzIKtsCK3vPfP17YQjmKggc"] = true,
+    ["JF9vOqNVxwU_PJUAg7Jc3xNg3WxzYfsJQ8jgBj1ddts"] = true,
+    ["B0Pfw_H6OjS_3PFH_ppBUa4ebMoxywohZEyArvw0NAo"] = true,
+    ["zCyGDe-gyARePupUr8t-kWdA2eIXhPkRmfO1tuzhPjA"] = true,
+    ["tG29nprqDSeo2Z3w0KaSxKBiBy55xZT3hPnMTI9cmVM"] = true
 }
 
 return {
